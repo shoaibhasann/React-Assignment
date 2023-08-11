43d4a1d51cea551643817fc3e3e6c534692ea84c
shoaibhasann <shoaibhasan0940@gmail.com>
2023-08-11 19:15:40 +0530

    Create navbar component

---
+++ Image-Gallery/src/components/Nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/Nav.css';
+
+function Nav() {
+  return (
+    <nav>
+      <div>
+        <h1>ReactGallery.</h1>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav
